02773c8f5920ac6cccc5d052570099da8af9549a
Pavel Timofeev <pavel.timofeev@hotmail.com>
2016-06-26 13:35:58 +0400

    grunt copy as is RestBehaviour

--- automation/cs-merger/Gruntfile.js
+++ automation/cs-merger/Gruntfile.js
@@ -10,8 +10,8 @@ module.exports = function(grunt) {
     },
     shell: {
       ps: {
-        options: {stdout: true},
-        command: 'powershell C:\\Git\\Repositories\\Gists\\CS-merger.ps1 -source C:\\Git\\Repositories\\UnityClient\\src\\UnityClient\\ -output C:\\Git\\Repositories\\UnityClient\\unity\\Assets\\RestBehaviour.cs -copyAsIs ScoreboardService >> C:\\Git\\Repositories\\UnityClient\\automation\\cs-merger\\cs-merger.log'
+        options: {stdout: true, stderr: true},
+        command: 'powershell C:\\Git\\Repositories\\Gists\\CS-merger.ps1 -source C:\\Git\\Repositories\\UnityClient\\src\\UnityClient\\ -output C:\\Git\\Repositories\\UnityClient\\unity\\Assets\\UnityClient.cs -copyAsIs ScoreboardService,RestBehaviour >> C:\\Git\\Repositories\\UnityClient\\automation\\cs-merger\\cs-merger.log'
       }
     }
   });
